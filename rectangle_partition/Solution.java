569898310feee66ebd385ab46c47fd51c068539e
Dominic Lynch <1968170+finchie@users.noreply.github.com>
2021-05-25 11:31:15 +0100

    add auto-generated Java code: Rectangle Partition

---
+++ rectangle_partition/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int w = in.nextInt();
+        int h = in.nextInt();
+        int countX = in.nextInt();
+        int countY = in.nextInt();
+        for (int i = 0; i < countX; i++) {
+            int x = in.nextInt();
+        }
+        for (int i = 0; i < countY; i++) {
+            int y = in.nextInt();
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("0");
+    }
+}
