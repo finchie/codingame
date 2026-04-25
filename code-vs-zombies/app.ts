e31ec3a84182cf56c3ba4f7fda248220aa6d244c
Dominic Lynch <1968170+finchie@users.noreply.github.com>
2026-04-25 22:42:57 +0100

    Add template start code for code-vs-zombies

---
+++ code-vs-zombies/app.ts
@@ -0,0 +1,33 @@
+/**
+ * Save humans, destroy zombies!
+ **/
+
+
+// game loop
+while (true) {
+    var inputs: string[] = readline().split(' ');
+    const x: number = parseInt(inputs[0]);
+    const y: number = parseInt(inputs[1]);
+    const humanCount: number = parseInt(readline());
+    for (let i = 0; i < humanCount; i++) {
+        var inputs: string[] = readline().split(' ');
+        const humanId: number = parseInt(inputs[0]);
+        const humanX: number = parseInt(inputs[1]);
+        const humanY: number = parseInt(inputs[2]);
+    }
+    const zombieCount: number = parseInt(readline());
+    for (let i = 0; i < zombieCount; i++) {
+        var inputs: string[] = readline().split(' ');
+        const zombieId: number = parseInt(inputs[0]);
+        const zombieX: number = parseInt(inputs[1]);
+        const zombieY: number = parseInt(inputs[2]);
+        const zombieXNext: number = parseInt(inputs[3]);
+        const zombieYNext: number = parseInt(inputs[4]);
+    }
+
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
+
+    console.log('0 0');     // Your destination coordinates
+
+}
