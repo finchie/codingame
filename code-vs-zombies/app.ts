5461e8f5f31945f516debe7175886f88a3c4ebf5
Dominic Lynch <1968170+finchie@users.noreply.github.com>
2026-04-26 00:10:26 +0100

    Add code to mimic Codingame `readline()`
    and tell typscript that inputs are not undefined

--- code-vs-zombies/app.ts
+++ code-vs-zombies/app.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+
 /**
  * Save humans, destroy zombies!
  **/
@@ -6,23 +8,23 @@
 // game loop
 while (true) {
     var inputs: string[] = readline().split(' ');
-    const x: number = parseInt(inputs[0]);
-    const y: number = parseInt(inputs[1]);
+    const x: number = parseInt(inputs[0]!);
+    const y: number = parseInt(inputs[1]!);
     const humanCount: number = parseInt(readline());
     for (let i = 0; i < humanCount; i++) {
         var inputs: string[] = readline().split(' ');
-        const humanId: number = parseInt(inputs[0]);
-        const humanX: number = parseInt(inputs[1]);
-        const humanY: number = parseInt(inputs[2]);
+        const humanId: number = parseInt(inputs[0]!);
+        const humanX: number = parseInt(inputs[1]!);
+        const humanY: number = parseInt(inputs[2]!);
     }
     const zombieCount: number = parseInt(readline());
     for (let i = 0; i < zombieCount; i++) {
         var inputs: string[] = readline().split(' ');
-        const zombieId: number = parseInt(inputs[0]);
-        const zombieX: number = parseInt(inputs[1]);
-        const zombieY: number = parseInt(inputs[2]);
-        const zombieXNext: number = parseInt(inputs[3]);
-        const zombieYNext: number = parseInt(inputs[4]);
+        const zombieId: number = parseInt(inputs[0]!);
+        const zombieX: number = parseInt(inputs[1]!);
+        const zombieY: number = parseInt(inputs[2]!);
+        const zombieXNext: number = parseInt(inputs[3]!);
+        const zombieYNext: number = parseInt(inputs[4]!);
     }
 
     // Write an action using console.log()
@@ -31,3 +33,14 @@ while (true) {
     console.log('0 0');     // Your destination coordinates
 
 }
+
+/**
+ * Mimics the readline() function in the CodinGame environment, allowing us to read input from a file instead of standard input. This is useful for testing and debugging our code locally before submitting it to CodinGame.
+ */
+const inputLines: string[] = fs.readFileSync(0, 'utf-8').trim().split('\n');
+let inputIndex = 0;
+
+function readline(): string {
+    return inputLines[inputIndex++] || '';
+}
+
